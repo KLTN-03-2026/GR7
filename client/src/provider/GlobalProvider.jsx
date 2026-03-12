8741d50714fc0652918355982a3e6f4c406251ab
NamHoang-Git <ngokhoangnam4268@gmail.com>
2026-03-12 22:16:07 +0700

    fix order api

--- client/src/provider/GlobalProvider.jsx
+++ client/src/provider/GlobalProvider.jsx
@@ -1,48 +1,16 @@
-import React, { createContext, useContext, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Axios from '../utils/Axios';
-import SummaryApi from '../common/SummaryApi';
-import AxiosToastError from '../utils/AxiosToastError';
-import { setOrder } from '../store/orderSlice';
+import React, { createContext, useContext } from 'react';
 
 export const GlobalContext = createContext(null);
 
 export const useGlobalContext = () => useContext(GlobalContext);
 
+// GlobalProvider – đã loại bỏ order ecommerce cũ
+// Chỉ giữ context stubs cho các legacy components không bị xóa kịp
 const GlobalProvider = ({ children }) => {
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state?.user);
-
-    const fetchOrder = async () => {
-        const accessToken = localStorage.getItem('accesstoken');
-        if (!accessToken || !user?._id) return;
-
-        try {
-            const response = await Axios({ ...SummaryApi.get_order_items });
-            const { data: responseData } = response;
-            if (responseData.success) {
-                dispatch(setOrder(responseData.data));
-            }
-        } catch (error) {
-            if (error?.response?.status !== 401) {
-                AxiosToastError(error);
-            }
-        }
-    };
-
-    useEffect(() => {
-        const accessToken = localStorage.getItem('accesstoken');
-        if (user?._id && accessToken) {
-            fetchOrder();
-        }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [user]);
-
     return (
         <GlobalContext.Provider
             value={{
-                fetchOrder,
-                // Stub values for legacy components that reference totalPrice/totalQty/etc.
+                // Stubs – legacy components có thể vẫn reference những này
                 totalPrice: 0,
                 totalQty: 0,
                 notDiscountTotalPrice: 0,
@@ -50,6 +18,7 @@ const GlobalProvider = ({ children }) => {
                 updateCartItem: () => {},
                 deleteCartItem: () => {},
                 reloadAfterPayment: () => {},
+                fetchOrder: () => {},
             }}
         >
             {children}
